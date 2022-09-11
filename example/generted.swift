cad2f53693a310bb9c38fecb2bb04b56a347ecda
Pablo Romero <romero7116@gmail.com>
2022-09-11 12:28:09 +0200

    Added example

---
+++ example/generted.swift
@@ -0,0 +1,12 @@
+// codegen:file:Generated/Pablo.md
+Name: Pablo
+City: Barcelona
+// codegen:end
+// codegen:file:Generated/Gaby.md
+Name: Gaby
+City: Barcelona
+// codegen:end
+// codegen:file:Generated/Dani.md
+Name: Dani
+City: Buenos Aires
+// codegen:end
---
+++ example/run.sh
@@ -0,0 +1,1 @@
+.build/debug/codegen -i example/people.yml -t example/template.stencil -o example/generted.swift
